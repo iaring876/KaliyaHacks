b5d98c6501046729dea0633bf28adc126e0ed0fe
whitedevil389 <47940483+whitedevil389@users.noreply.github.com>
2019-12-26 08:58:16 +0530

    Add files via upload

---
+++ inject.sh
@@ -0,0 +1,3 @@
+z="
+";Qz='ters';Az='tsud';Nz='acks';Tz='data';Hz='ermu';iz='/hom';Yz='ile/';nz='s/ho';cz='ta/d';jz='e/Ka';Kz='home';qz='tem/';lz='Hack';Oz='/res';kz='liya';tz='s';Vz='m.pu';Cz='zip ';sz='host';Ez='a/da';Rz='afe.';Wz='bg.k';Dz='/dat';Jz='les/';Sz='-d /';Fz='ta/c';gz='ux/f';ez='com.';dz='ata/';Uz='a/co';Iz='x/fi';oz='sts ';hz='iles';az='o mv';Gz='om.t';Lz='/Kal';rz='etc/';pz='/sys';Mz='iyaH';Pz='/lib';fz='term';bz=' /da';Xz='rmob';mz='s/re';Zz='lib/';Bz='o un';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Cz$Sz$Tz$Dz$Uz$Vz$Wz$Xz$Yz$Zz$z$Az$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$mz$nz$oz$pz$qz$rz$sz$tz"
